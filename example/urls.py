89f662d723bacef8af47558fca28de52b57b95d5
Taoxin Jiang <universalkbcontrol@gmail.com>
2023-06-24 18:52:02 -0400

    change urlpatterns

--- example/urls.py
+++ example/urls.py
@@ -1,8 +1,10 @@
 # example/urls.py
+from django.contrib import admin
 from django.urls import path
 from example.views import index, upload_file
 
 urlpatterns = [
     #path('', index),
-    path('', upload_file),
+    path('admin/', admin.site.urls),
+    path('upload/', upload_file),
 ]
