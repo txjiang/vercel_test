b2ea9c11ee7597221db3f89683712dff50845f7c
Taoxin Jiang <universalkbcontrol@gmail.com>
2023-06-30 20:58:35 -0400

    change latex to pdf

--- example/views.py
+++ example/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
-from vercel_app.process_file import create_latex_file
+from vercel_app.process_file import create_latex_file, create_pdf_file
 
 def index(request):
     now = datetime.now()
@@ -21,7 +21,7 @@ def index(request):
 def process_the_upload_file(request):
     if request.method == 'POST':
         file = request.FILES['file']
-        processed_file = create_latex_file()
+        processed_file = create_pdf_file()
         response = HttpResponse(content_type='application/octet-stream')
         response['Content-Disposition'] = 'attachment; filename="processed_file.tex"'
         response.write(processed_file.getvalue())
--- vercel_app/process_file.py
+++ vercel_app/process_file.py
@@ -1,8 +1,22 @@
 import io
+from reportlab.pdfgen import canvas
 
 def create_latex_file() -> bytes:
     latex_content = "\\documentclass{article}\n\\begin{document}\nThis is the content of the LaTeX file.\n\\end{document}"
     file_obj = io.BytesIO()
     file_obj.write(latex_content.encode())
     file_obj.seek(0)
+    return file_obj
+
+def create_pdf_file():
+    file_obj = io.BytesIO()
+    p = canvas.Canvas(file_obj)
+
+    # Add content to the PDF
+    p.drawString(100, 100, "This is the content of the PDF file.")
+    
+    p.showPage()
+    p.save()
+    file_obj.seek(0)
+
     return file_obj
